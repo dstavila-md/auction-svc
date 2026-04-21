40008abbcb1363f3b510931c8a05f323e4a6f01d
XenderMd <denis.stavilamd@gmail.com>
2026-04-21 18:32:15 +0300

    adding the update auction endpoint + bugfix

--- AuctionService/src/AuctionService/Controllers/AuctionsController.cs
+++ AuctionService/src/AuctionService/Controllers/AuctionsController.cs
@@ -46,4 +46,25 @@ public class AuctionsController : ControllerBase
 		return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, this._mapper.Map<AuctionDto>(auction));
 	}
 
+	[HttpPut("{id}")]
+	public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+	{
+		var auction = await this._context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+		if (auction == null) return NotFound();
+
+		// TODO: check seller is current user
+
+		auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+		auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+		auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+		auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+		auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+
+		var result = await this._context.SaveChangesAsync() > 0;
+
+		if (result) return Ok();
+
+		return BadRequest("Problem saving changes");
+	}
+
 }
--- AuctionService/src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ AuctionService/src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -3,14 +3,13 @@ namespace AuctionService.DTOs;
 
 public class UpdateAuctionDto
 {
-	[Required]
 	public string Make { get; set; }
-	[Required]
+
 	public string Model { get; set; }
-	[Required]
-	public int Year { get; set; }
-	[Required]
+
+	public int? Year { get; set; }
+
 	public string Color { get; set; }
-	[Required]
-	public int Mileage { get; set; }
+
+	public int? Mileage { get; set; }
 }
